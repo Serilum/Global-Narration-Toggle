f16868dafa81d9c4322eb8e6d13f70fb8ce78556
Rick South <rick@natamus.com>
2023-12-18 10:26:26 +0100

    (v1.1) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/globalnarrationtoggle/util/Reference.java
+++ Common/src/main/java/com/natamus/globalnarrationtoggle/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.globalnarrationtoggle.util;
 public class Reference {
 	public static final String MOD_ID = "globalnarrationtoggle";
 	public static final String NAME = "Global Narration Toggle";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }
