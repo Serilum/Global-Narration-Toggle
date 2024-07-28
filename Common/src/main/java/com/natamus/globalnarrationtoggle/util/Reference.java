293a43f10d2044299be78b1ca765b78fbaf25cb3
Rick South <rick@natamus.com>
2024-07-28 07:30:54 +0200

    (v1.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/globalnarrationtoggle/util/Reference.java
+++ Common/src/main/java/com/natamus/globalnarrationtoggle/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.globalnarrationtoggle.util;
 public class Reference {
 	public static final String MOD_ID = "globalnarrationtoggle";
 	public static final String NAME = "Global Narration Toggle";
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "1.4";
 	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
 }
