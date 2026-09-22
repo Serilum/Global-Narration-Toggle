b34bf4efdf9adf4bbf73c666ded1e79d5b304ce5
Rick South <rick@natamus.com>
2026-09-22 16:41:41 +0200

    Updated Global Narration Toggle to Minecraft version 26.3.0.

--- Common/src/main/java/com/natamus/globalnarrationtoggle/util/Reference.java
+++ Common/src/main/java/com/natamus/globalnarrationtoggle/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "globalnarrationtoggle";
 	public static final String NAME = "Global Narration Toggle";
 	public static final String VERSION = "1.4";
-	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
+	public static final String ACCEPTED_VERSIONS = "[26.3.0]";
 }
